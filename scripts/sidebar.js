c5aa938152cd00b47ab35fef4edd9a7f3e469867
GokulVSD <gokul.vasda@gmail.com>
2018-08-15 01:57:28 +0530

    Removed unecessary scroll block

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,14 +1,5 @@
-var disableScroll = false;
-
 $('#sidebarCollapse').click(function() {
 
     $("#wrapper").toggleClass("toggled");
     $(this).toggleClass("active");
-    disableScroll = !disableScroll;
-});
-
-$('#wrapper').on("touchmove", function(e){
-	
-	if(disableScroll)
-		e.preventDefault();
 });
