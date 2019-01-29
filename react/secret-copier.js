2b0834b534cdfe988e1813ac5e2d34bb09ffec3b
GokulVSD <gokul.vasda@gmail.com>
2019-01-29 22:14:44 +0530

    More UI improvements

--- react/secret-copier.js
+++ react/secret-copier.js
@@ -5,24 +5,27 @@ class SecretCopier extends React.Component {
         super(props);
     }
 
-    copyContent(){
+    copyContent() {
 
         var text = this.props.curr + "|Y|" + this.props.sign + this.props.ticketAppend;
-        
+
         // clipboard API
         navigator.clipboard.writeText(text);
-        
+
         // If called within a generator
-        if(this.props.incAndUpdate !== undefined){
-            
+        if (this.props.incAndUpdate !== undefined) {
+
             this.props.incAndUpdate();
         }
     }
 
     render() {
         return e("div",
-            { onClick: () => this.copyContent() },
-            this.props.curr + "|Y|" + this.props.sign);
+            { className: "input-container wide secret", onClick: () => this.copyContent() },
+            e("div", null,
+                this.props.curr, e("img", { src: "assets/logo-ticket.png" }, null), this.props.sign
+            )
+        );
     }
 }
 
