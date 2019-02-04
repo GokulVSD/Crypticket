9e9e6f228706ebf81598ee7829d96d38f6ca34b7
GokulVSD <gokul.vasda@gmail.com>
2019-02-04 21:56:02 +0530

    minor cosmetic change

--- react/passwords-tab.js
+++ react/passwords-tab.js
@@ -100,7 +100,7 @@ class PasswordsTab extends React.Component {
         password.appNames.map(appName => {
           return e(React.Fragment, { key: appName + password.password },
 
-            e("div", { className: "input-container wide cpr pswdinp", 'data-tip': "Copied!", 'data-event': 'click', 'data-event-off' : 'mouseout', 'data-for': 'tt2' },
+            e("div", { className: "input-container wide cpr pswdinp", 'data-tip': "Copied!", 'data-event': 'click', 'data-event-off' : 'nothing', 'data-for': 'tt2' },
               e(Copier,
                 { title: appName, content: hexToBase64(getECDSAKey(password.password).sign(appName.toUpperCase()).toHex()).slice(0, 12).replace(/\//g, "x") + "+0Pw" },
                 null),
